60118eb1bbfdfde39ed143076d5166f8288f6a7a
Pavel Vařenka <pvarenka@gwi.com>
2023-08-22 18:44:38 +0200

    add fetcher

--- parser/clusters.go
+++ parser/clusters.go
@@ -1,62 +1,69 @@
+// parser/clusters.go
 package parser
 
 import (
-	container "cloud.google.com/go/container/apiv1"
-	containerpb "cloud.google.com/go/container/apiv1/containerpb"
-	"context"
-	"fmt"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/kallakata/k8s_cli/model"
-	"github.com/kallakata/k8s_cli/pretty/pretty_clusters"
-	"time"
+    "context"
+    "fmt"
+    "time"
+    "github.com/kallakata/k8s_cli/internal/interfaces"
+    "github.com/kallakata/k8s_cli/model"
+    "cloud.google.com/go/container/apiv1"
+    "cloud.google.com/go/container/apiv1/containerpb"
 )
 
-func ListClusters(projectID, zone string) ([]model.Cluster, error) {
-	ctx := context.Background()
+func ListClusters(projectID, zone string, npf interfaces.NodePoolsFetcher) ([]model.Cluster, error) {
+    ctx := context.Background()
 
-	c, err := container.NewClusterManagerClient(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("Could not initialize gke client: %v", err)
-	}
+    c, err := container.NewClusterManagerClient(ctx)
+    if err != nil {
+        return nil, fmt.Errorf("Could not initialize gke client: %v", err)
+    }
+    defer c.Close()
 
-	defer c.Close()
+    req := &containerpb.ListClustersRequest{
+        ProjectId: projectID,
+        Zone:      zone,
+    }
+    resp, err := c.ListClusters(ctx, req)
+    if err != nil {
+        return nil, fmt.Errorf("Error listing clusters: %v", err)
+    }
 
-	req := &containerpb.ListClustersRequest{
-		ProjectId: projectID,
-		Zone:      zone,
-	}
-	resp, err := c.ListClusters(ctx, req)
-	if err != nil {
-		return nil, fmt.Errorf("Error listing clusters: %v", err)
-	}
+    var clusters []model.Cluster
+    for _, c := range resp.Clusters {
+        var status model.ClusterStatus
 
-	var clusters []model.Cluster
-	for _, c := range resp.Clusters {
-		cluster := model.Cluster{
-			Cluster:  c.Name,
-			Status:   string(c.Status),
-			Version:  c.CurrentMasterVersion,
-			Endpoint: c.Endpoint,
-		}
-		clusters = append(clusters, cluster)
-	}
+        switch c.Status {
+        case 0:
+            status = model.StatusUnspecified
+        case 1:
+            status = model.Provisioning
+        case 2:
+            status = model.Running
+        case 3:
+            status = model.Reconciling
+        case 4:
+            status = model.Stopping
+        case 5:
+            status = model.Error
+        case 6:
+            status = model.Degraded
+        default:
+            status = model.StatusUnspecified // Default to unspecified if not recognized
+        }
 
-	p := tea.NewProgram(pretty_clusters.NewModel(clusters))
-	fmt.Printf("========== Getting clusters ==========\n\n")
-	time.Sleep(2 * time.Second)
-	p.Run()
+    cluster := model.Cluster{
+        Cluster:  c.Name,
+        Status:   status,
+        Version:  c.CurrentMasterVersion,
+        Endpoint: c.Endpoint,
+    }
 
-	// np := tea.NewProgram(pretty.NewNodepoolsModel(nodepools))
-	// fmt.Printf("========== Getting nodepools ==========\n\n")
-	// time.Sleep(2 * time.Second)
-	// np.Run()
+    clusters = append(clusters, cluster)
+}
 
-	for _, cluster := range resp.Clusters {
-		fmt.Printf("Cluster Name: %s\n", cluster.Name)
-		fmt.Printf("Cluster Status: %s\n", cluster.Status)
-		fmt.Printf("Cluster Version: %s\n", cluster.CurrentMasterVersion)
-		fmt.Printf("Cluster Endpoint: %s\n", cluster.Endpoint)
-		fmt.Println("----------------------")
-	}
-	return clusters, nil
+    fmt.Printf("========== Getting clusters ==========\n\n")
+    time.Sleep(2 * time.Second)
+    // Instead of directly running the program, return the clusters
+    return clusters, nil
 }
---
+++ parser/nodepools_fetcher.go
@@ -0,0 +1,48 @@
+package parser
+
+import (
+	"fmt"
+    "github.com/kallakata/k8s_cli/model"
+	container "cloud.google.com/go/container/apiv1"
+	containerpb "cloud.google.com/go/container/apiv1/containerpb"
+	"github.com/kallakata/k8s_cli/internal/interfaces"
+	"context"
+)
+
+type nodePoolsFetcher struct{}
+
+func (npf nodePoolsFetcher) FetchNodePoolsForCluster(projectID, zone, clusterName string) ([]model.Nodepool, error) {
+    ctx := context.Background()
+
+    c, err := container.NewClusterManagerClient(ctx)
+    if err != nil {
+        return nil, fmt.Errorf("Could not initialize gke client: %v", err)
+    }
+    defer c.Close()
+
+    req := &containerpb.ListNodePoolsRequest{
+        Parent: fmt.Sprintf("projects/%s/locations/%s/clusters/%s", projectID, zone, clusterName),
+    }
+    resp, err := c.ListNodePools(ctx, req)
+    if err != nil {
+        return nil, fmt.Errorf("Error listing node pools: %v", err)
+    }
+
+    var nodePools []model.Nodepool
+    for _, np := range resp.NodePools {
+        nodePool := model.Nodepool{
+            Nodepool:   np.Name,
+            Status: string(np.Status),
+			Version: np.Version,
+			Autoscaling: np.Autoscaling.Enabled,
+			MinNode:     np.Autoscaling.MinNodeCount,
+			MaxNode:     np.Autoscaling.MaxNodeCount,
+        }
+        nodePools = append(nodePools, nodePool)
+    }
+
+    return nodePools, nil
+}
+
+// Create an instance of the interface for external use
+var NodePoolsFetcherInstance interfaces.NodePoolsFetcher = nodePoolsFetcher{}
