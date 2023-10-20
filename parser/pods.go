98e93c7367f434768bb43e92ba0b65eb2f0c6feb
Pavel Vařenka <pavel.varenka@ataccama.com>
2023-10-20 17:23:13 +0200

    new bin

--- parser/pods.go
+++ parser/pods.go
@@ -25,17 +25,17 @@ func ListPods(ns string, ctx string) ([]model.Pod, *kubernetes.Clientset, error)
 	kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
 
 	var clientset *kubernetes.Clientset
-    if ctx == "" {
-        // If ctx is not provided as an argument, get the current Kubernetes context
-        kubeConfig, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
-            &clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeConfigPath},
-            &clientcmd.ConfigOverrides{}).RawConfig()
-        if err != nil {
-            fmt.Printf("Error getting kubernetes config: %v\n", err)
-            os.Exit(1)
-        }
-        ctx = kubeConfig.CurrentContext
-    }
+	if ctx == "" {
+		// If ctx is not provided as an argument, get the current Kubernetes context
+		kubeConfig, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+			&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeConfigPath},
+			&clientcmd.ConfigOverrides{}).RawConfig()
+		if err != nil {
+			fmt.Printf("Error getting kubernetes config: %v\n", err)
+			os.Exit(1)
+		}
+		ctx = kubeConfig.CurrentContext
+	}
 	configLoadingRules := &clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeConfigPath}
 	configOverrides := &clientcmd.ConfigOverrides{CurrentContext: ctx}
 
@@ -60,7 +60,7 @@ func ListPods(ns string, ctx string) ([]model.Pod, *kubernetes.Clientset, error)
 	if err != nil {
 		os.Exit(1)
 	}
-	
+
 	var items []model.Pod
 
 	if ns == "" {
@@ -69,11 +69,11 @@ func ListPods(ns string, ctx string) ([]model.Pod, *kubernetes.Clientset, error)
 				Pod:       pod.Name,
 				Status:    string(pod.Status.Phase),
 				Namespace: pod.Namespace,
-				CPUReq: pod.Spec.Containers[0].Resources.Requests.Cpu().String(),
-				CPULim: pod.Spec.Containers[0].Resources.Limits.Cpu().String(),
-				MemReq: pod.Spec.Containers[0].Resources.Requests.Memory().String(),
-				MemLim: pod.Spec.Containers[0].Resources.Limits.Memory().String(),
-				Image: pod.Spec.Containers[0].Image,
+				CPUReq:    pod.Spec.Containers[0].Resources.Requests.Cpu().String(),
+				CPULim:    pod.Spec.Containers[0].Resources.Limits.Cpu().String(),
+				MemReq:    pod.Spec.Containers[0].Resources.Requests.Memory().String(),
+				MemLim:    pod.Spec.Containers[0].Resources.Limits.Memory().String(),
+				Image:     pod.Spec.Containers[0].Image,
 				Context:   ctx,
 			}
 			items = append(items, item)
@@ -85,11 +85,11 @@ func ListPods(ns string, ctx string) ([]model.Pod, *kubernetes.Clientset, error)
 				Pod:       pod.Name,
 				Status:    string(pod.Status.Phase),
 				Namespace: ns,
-				CPUReq: pod.Spec.Containers[0].Resources.Requests.Cpu().String(),
-				CPULim: pod.Spec.Containers[0].Resources.Limits.Cpu().String(),
-				MemReq: pod.Spec.Containers[0].Resources.Requests.Memory().String(),
-				MemLim: pod.Spec.Containers[0].Resources.Limits.Memory().String(),
-				Image: pod.Spec.Containers[0].Image,
+				CPUReq:    pod.Spec.Containers[0].Resources.Requests.Cpu().String(),
+				CPULim:    pod.Spec.Containers[0].Resources.Limits.Cpu().String(),
+				MemReq:    pod.Spec.Containers[0].Resources.Requests.Memory().String(),
+				MemLim:    pod.Spec.Containers[0].Resources.Limits.Memory().String(),
+				Image:     pod.Spec.Containers[0].Image,
 				Context:   ctx,
 			}
 			items = append(items, item)
@@ -97,20 +97,19 @@ func ListPods(ns string, ctx string) ([]model.Pod, *kubernetes.Clientset, error)
 	}
 
 	p := tea.NewProgram(pretty_pods.NewModel(items, ctx, ns))
-	fmt.Printf("\n\n========== | Getting pods | ==========\n\n")
+	fmt.Printf("\n\n========== | Getting pods... | ==========\n\n")
 	time.Sleep(1 * time.Second)
 	p.Run()
 
 	return items, clientset, nil
 }
 
-func ListPodsUsingPrompt(ctx string) (error) {
+func ListPodsUsingPrompt(ctx string) error {
 
-	// Create a new prompt model and run it
 	promptModel := prompt_pods.InitialModel()
 	promptProgram := tea.NewProgram(promptModel)
 	resultMsg, _ := promptProgram.Run()
-	
+
 	// Check if the result message is a model with a GetNamespace method
 	if resultMsg != nil {
 		if namespaceModel, ok := resultMsg.(interface{ GetNamespace() string }); ok {
